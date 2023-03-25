6e34959a148efb9d34c1405ddd8078908bbaa608
Emete Gift <emeteokeoghene@gmail.com>
2023-03-25 11:28:37 +0100

    Update app.run() configuration

--- app.py
+++ app.py
@@ -78,3 +78,7 @@ def create_app(db_url=None):
 #     app.run(debug=True)
 
     return app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
